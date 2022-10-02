12ae2ea7af7600084e0fabe52130598ab54c1d0a
Mike-95 <shokhasoft@gmail.com>
2022-10-02 15:41:38 +0400

    done binding of summary fragment

--- app/src/main/java/com/example/macdonaldapp/model/OrderViewModel.kt
+++ app/src/main/java/com/example/macdonaldapp/model/OrderViewModel.kt
@@ -8,16 +8,16 @@ import java.util.*
 
 class OrderViewModel : ViewModel() {
 
-    private val _quantity = MutableLiveData<Int>(0)
+    private val _quantity = MutableLiveData<Int>()
     val quantity: LiveData<Int> = _quantity
 
-    private val _flavor = MutableLiveData<String>("")
+    private val _flavor = MutableLiveData<String>()
     val flavor: LiveData<String> = _flavor
 
-    private val _data = MutableLiveData<String>("")
+    private val _data = MutableLiveData<String>()
     val data: LiveData<String> = _data
 
-    private val _price = MutableLiveData<Double>(0.0)
+    private val _price = MutableLiveData<Double>()
     val price: LiveData<Double> = _price
 
     // Methods
@@ -33,17 +33,21 @@ class OrderViewModel : ViewModel() {
         _data.value = pickupDate
     }
 
+    init {
+        resetOrder()
+    }
+
     // Checking if the flavor for  order has been set or not
     fun hasNoFlavorSet(): Boolean {
         return _flavor.value.isNullOrEmpty()
     }
 
     // Creating list of pickup dates
-    private fun getPickupOptions(): List<String>{
+    private fun getPickupOptions(): List<String> {
         val options = mutableListOf<String>()
         val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
         val calendar = Calendar.getInstance()
-        repeat(4){
+        repeat(4) {
             options.add(formatter.format(calendar.time))
             calendar.add(Calendar.DATE, 1)
         }
@@ -52,5 +56,11 @@ class OrderViewModel : ViewModel() {
 
     val dateOptions = getPickupOptions()
 
-
+    // reset Mutable Live data properties
+    private fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+     //  _data.value = dateOptions[1]
+        _price.value = 0.0
+    }
 }
